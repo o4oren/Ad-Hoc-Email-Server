bcc4a2d50fc38099b6e7499c70dfaecd7b31c6c7
Oren Geva <o4oren@gmail.com>
2017-07-15 20:21:31 +0300

    remove style from html parsing

--- client/app/pipe/html-cleanup.pipe.ts
+++ client/app/pipe/html-cleanup.pipe.ts
@@ -10,6 +10,9 @@ export class HtmlCleanupPipe implements PipeTransform {
     if(html.includes("<html") && html.includes("<body")) {
       let part1 = html.split('<body')[1];
       let part2 = part1.slice(part1.indexOf('>') + 1);
+      if(part2.startsWith('<style')) {
+        part2 = part2.slice(part2.indexOf('<style'), part2.indexOf('</style>'));
+      }
       let part3 = part2.split('</body>')[0];
       return part3;
     }
