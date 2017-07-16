ea991fac4ef80ee98651d066dc82fa821da493f7
Oren Geva <ogeva@blackberry.com>
2017-07-16 10:06:25 +0300

    Add removal of style tag to htmlCleanupPipe

--- client/app/pipe/html-cleanup.pipe.ts
+++ client/app/pipe/html-cleanup.pipe.ts
@@ -10,9 +10,9 @@ export class HtmlCleanupPipe implements PipeTransform {
     if(html.includes("<html") && html.includes("<body")) {
       let part1 = html.split('<body')[1];
       let part2 = part1.slice(part1.indexOf('>') + 1);
-      // if(part2.startsWith('<style')) {
-      //   part2 = part2.split('</style>')[1];
-      // }
+      if(part2.startsWith('<style')) {
+        part2 = part2.split('</style>')[1];
+      }
       let part3 = part2.split('</body>')[0];
       console.log('parsed html', part3)
       return part3;
