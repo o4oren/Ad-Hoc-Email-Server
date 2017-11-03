30b1e26127fc18a399caa7ef6adab6355204a481
Oren Geva <ogeva@blackberry.com>
2017-11-03 23:36:26 +0200

    Replacing . in keys

--- server/app/smtp.js
+++ server/app/smtp.js
@@ -38,6 +38,8 @@ module.exports = {
 
           simpleParser(mailDataString, (err, mail) => {
             mail.timestamp = new Date().getTime();
+            //replace all . in the header keys due to insertion probelm
+            mail.headers.forEach(value, key, map => { key=key.replace(/\./g,'_') });
             db.collection('emails').insertOne(mail, function (err, result) {
 
               if (err) {
