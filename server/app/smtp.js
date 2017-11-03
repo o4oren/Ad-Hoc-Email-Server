de515676f1c8ed63974cb9896deac0016fb6bb4b
Oren Geva <ogeva@blackberry.com>
2017-11-04 00:45:59 +0200

    remove )

--- server/app/smtp.js
+++ server/app/smtp.js
@@ -12,86 +12,86 @@ module.exports = {
   startSTMPServer: function startSTMPServer(properties, baseDir, db) {
     const smtpPort = properties.smtpPort;
     const mailserver = new SMTPServer({
-      logger: true,
-      authOptional: true,
-      disabledCommands: ['AUTH'],
-      disableReverseLookup: true,
-      onConnect(session, callback){
-        log.info("Connection started.")
-        return callback(); // Accept the connection
-      },
-      onRcptTo(address, session, callback){
-        if (!validateAddress(address, properties.allowedDomains)) {
-          return callback(new Error('Only the domains ' + [JSON.stringify(properties.allowedDomains)] + ' are allowed to receive mail'));
-        }
-        return callback(); // Accept the address
-      },
-      onData(stream, session, callback){
-        let mailDataString = '';
-        const rcptTo = session.envelope.rcptTo;
+        logger: true,
+        authOptional: true,
+        disabledCommands: ['AUTH'],
+        disableReverseLookup: true,
+        onConnect(session, callback){
+          log.info("Connection started.")
+          return callback(); // Accept the connection
+        },
+        onRcptTo(address, session, callback){
+          if (!validateAddress(address, properties.allowedDomains)) {
+            return callback(new Error('Only the domains ' + [JSON.stringify(properties.allowedDomains)] + ' are allowed to receive mail'));
+          }
+          return callback(); // Accept the address
+        },
+        onData(stream, session, callback){
+          let mailDataString = '';
+          const rcptTo = session.envelope.rcptTo;
 
-        stream.on("data", function (chunk) {
-          mailDataString += chunk;
-        });
-
-        stream.on("end", function () {
-
-          simpleParser(mailDataString, (err, mail) => {
-            mail.timestamp = new Date().getTime();
-            //replace all . in the header keys due to insertion probelm
-            Object.keys(mail.headers).forEach(function (key) {
-              if (key.includes('.')) {
-                let newKey = key.replace('/\./g', '_');
-                mail.headers[newkey] = mail.headers[key];
-                delete mail.headers[key];
-              }
-            });
+          stream.on("data", function (chunk) {
+            mailDataString += chunk;
           });
-          console.log('headers', mail.headers);
-          db.collection('emails').insertOne(mail, function (err, result) {
 
-            if (err) {
-              console.error("Error in writing email", err);
-              return;
-            }
+          stream.on("end", function () {
 
-            mail.to.value.forEach(recipient => {
-              let nameAndDomain = recipient.address.split('@');
-              if (properties.allowedDomains.indexOf(nameAndDomain[1].toLowerCase()) > -1) {
-                db.collection('accounts').updateOne({"name": nameAndDomain[0]}, {
-                  $push: {
-                    "emails": {
-                      "emailId": mail._id,
-                      "sender": mail.from.value[0],
-                      "subject": mail.subject,
-                      "timestamp": mail.timestamp
-                    }
+            simpleParser(mailDataString, (err, mail) => {
+              mail.timestamp = new Date().getTime();
+              //replace all . in the header keys due to insertion probelm
+              Object.keys(mail.headers).forEach(function (key) {
+                if (key.includes('.')) {
+                  let newKey = key.replace('/\./g', '_');
+                  mail.headers[newkey] = mail.headers[key];
+                  delete mail.headers[key];
+                }
+              });
+
+              console.log('headers', mail.headers);
+              db.collection('emails').insertOne(mail, function (err, result) {
+
+                if (err) {
+                  console.error("Error in writing email", err);
+                  return;
+                }
+
+                mail.to.value.forEach(recipient => {
+                  let nameAndDomain = recipient.address.split('@');
+                  if (properties.allowedDomains.indexOf(nameAndDomain[1].toLowerCase()) > -1) {
+                    db.collection('accounts').updateOne({"name": nameAndDomain[0]}, {
+                      $push: {
+                        "emails": {
+                          "emailId": mail._id,
+                          "sender": mail.from.value[0],
+                          "subject": mail.subject,
+                          "timestamp": mail.timestamp
+                        }
+                      }
+                    }, {upsert: true}, function (err, res) {
+                      if (err)
+                        console.error("Error in writing to account", err);
+                      console.log("Inserted results into the collection.");
+                    });
                   }
-                }, {upsert: true}, function (err, res) {
-                  if (err)
-                    console.error("Error in writing to account", err);
-                  console.log("Inserted results into the collection.");
+
                 });
-              }
-
+              });
             });
+            callback();
           });
-        });
-        callback();
-      });
+        }
+      }
+      )
+    ;
+
+    mailserver.on('error', err => {
+      log.error('Error %s', err.message);
+    });
+
+    mailserver.listen(smtpPort);
+
+    return mailserver;
   }
-}
-)
-;
-
-mailserver.on('error', err => {
-  log.error('Error %s', err.message);
-});
-
-mailserver.listen(smtpPort);
-
-return mailserver;
-}
 }
 
 function validateAddress(address, allowedDomains) {
