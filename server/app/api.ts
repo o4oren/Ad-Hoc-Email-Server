f934e99ddbbf20594bdb3a46777a6b921b026b4f
Oren Geva <o4oren@gmail.com>
2017-11-20 08:06:26 +0200

    fix linting on serever

--- server/app/api.ts
+++ server/app/api.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 const ObjectID = require('mongodb').ObjectID;
 
 
-//indicates the api server is up
+// indicates the api server is up
 router.get('/alive', (req, res) => {
   res.send('api works');
 });
@@ -15,9 +15,11 @@ router.get('/alive', (req, res) => {
  * returns a list of account names starting with the req.body.prefix
  */
 router.post('/account/autocomplete', (req, res) => {
-  req.db.collection("accounts").find({"name": {'$regex' : '^' + req.body.prefix, '$options' : 'i'}}, {"name": 1}).toArray(function (err, accounts) {
-    if (err)
+  req.db.collection('accounts').find({'name': {'$regex' : '^' + req.body.prefix, '$options' : 'i'}},
+    {'name': 1}).toArray(function (err, accounts) {
+    if (err) {
       res.status(500).json(err);
+    }
     res.status(200).send(accounts.map(account => account.name));
   });
 });
@@ -35,11 +37,13 @@ router.get('/properties', (req, res, next) => {
  * returns a list of mail metadata bojects in a specific account
  */
 router.get('/account/:account', (req, res, next) => {
-  req.db.collection("accounts").findOne({"name": req.params.account}, function (err, account) {
-    if (err)
+  req.db.collection('accounts').findOne({'name': req.params.account}, function (err, account) {
+    if (err) {
       return res.status(500).json(err);
-    if (!account || account.emails.length === 0)
-      return res.status(404).send({error: "ACCOUNT IS EMPTY!"});
+    }
+    if (!account || account.emails.length === 0) {
+      return res.status(404).send({error: 'ACCOUNT IS EMPTY!'});
+    }
     res.status(200).send(account.emails);
   });
 
@@ -50,20 +54,21 @@ router.get('/account/:account', (req, res, next) => {
  */
 router.get('/account/:account/:emailId', (req, res) => {
 
-  let objectId = ObjectID.createFromHexString(req.params.emailId);
-  req.db.collection("emails").findOne({"_id": objectId}, {
-    "from": 1,
-    "to": 1,
-    "cc": 1,
-    "date": 1,
-    "timestamp":1,
-    "subject": 1,
-    "html": 1,
-    "textAsHtml": 1,
-    "attachments.filename": 1
-  },function (err, doc) {
-    if(err)
+  const objectId = ObjectID.createFromHexString(req.params.emailId);
+  req.db.collection('emails').findOne({'_id': objectId}, {
+    'from': 1,
+    'to': 1,
+    'cc': 1,
+    'date': 1,
+    'timestamp': 1,
+    'subject': 1,
+    'html': 1,
+    'textAsHtml': 1,
+    'attachments.filename': 1
+  }, function (err, doc) {
+    if (err) {
       res.status(500).send({error: err});
+    }
     res.status(200).send(doc);
   });
 });
@@ -73,12 +78,13 @@ router.get('/account/:account/:emailId', (req, res) => {
  */
 router.patch('/account/:account/:emailId', (req, res) => {
 
-  let objectId = ObjectID.createFromHexString(req.params.emailId);
-  req.db.collection("accounts").updateOne({ "name": req.params.account, "emails.emailId" : objectId},
-    {$set: {"emails.$.isRead": req.body.isRead}},
+  const objectId = ObjectID.createFromHexString(req.params.emailId);
+  req.db.collection('accounts').updateOne({ 'name': req.params.account, 'emails.emailId' : objectId},
+    {$set: {'emails.$.isRead': req.body.isRead}},
     function (err, result) {
-    if(err)
+    if (err) {
       res.status(500).send({error: err});
+    }
     res.status(200).send(result);
   });
 });
@@ -89,10 +95,10 @@ router.patch('/account/:account/:emailId', (req, res) => {
 router.get('/account/:account/:emailId/attachments/:filename', (req, res) => {
   try {
 
-    let objectId = ObjectID.createFromHexString(req.params.emailId);
-    req.db.collection("emails").findOne({"_id": objectId}, function(err, mail) {
-      let attachmentsFound = mail.attachments.filter(attachment => {
-        return attachment.filename == decodeURI(req.params.filename);
+    const objectId = ObjectID.createFromHexString(req.params.emailId);
+    req.db.collection('emails').findOne({'_id': objectId}, function(err, mail) {
+      const attachmentsFound = mail.attachments.filter(attachment => {
+        return attachment.filename === decodeURI(req.params.filename);
       });
       console.log(attachmentsFound);
       res.setHeader('Content-Type', attachmentsFound[0].contentType);
@@ -102,29 +108,29 @@ router.get('/account/:account/:emailId/attachments/:filename', (req, res) => {
       res.end(attachmentsFound[0].content.buffer);
       }
     );
-  }
-  catch (e) {
+  } catch (e) {
     console.log(e);
-    res.status(404).send({error: "FILE NOT FOUND"});
+    res.status(404).send({error: 'FILE NOT FOUND'});
   }
 });
 
 router.delete('/account/:account/:emailId', (req, res) => {
-  let objectId = ObjectID.createFromHexString(req.params.emailId);
-  req.db.collection("accounts").updateOne(
-    { "name" : req.params.account },
-    {$pull : {"emails" : {"emailId":objectId}}}
-    ,function (err, result) {
-      if(err)
+  const objectId = ObjectID.createFromHexString(req.params.emailId);
+  req.db.collection('accounts').updateOne(
+    { 'name' : req.params.account },
+    {$pull : {'emails' : {'emailId': objectId}}}
+    , function (err, result) {
+      if (err) {
         res.status(500).send({error: err});
+      }
       res.json({success: true});
 
     }
-  )
+  );
 });
 
 router.delete('/account/:account', (req, res) => {
-  req.db.collection("accounts").remove({"name":req.params.account}, function(err, result){
+  req.db.collection('accounts').remove({'name': req.params.account}, function(err, result){
     if (err) {
       res.status(500).send({error: err, succes: false});
     }
