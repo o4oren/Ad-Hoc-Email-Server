d4fc80582737cf5718de8ec38aac927cbfce6ffb
ogeva <ogeva@blackberry.com>
2018-09-26 15:35:03 +0300

    tslinked EmailDetails info

--- client/app/model/email-details-model.ts
+++ client/app/model/email-details-model.ts
@@ -1,40 +1,38 @@
-/**
- * Created by ogeva on 7/6/2017.
- */
+
 
 export interface EmailDetails {
-  "_id": string,
-  "timestamp": Number,
-  "attachments": Array<any>,
-  "headers": any,
-  "html": string,
-  "text":string,
-  "textAsHtml":string,
-  "subject":string,
-  "date":string,
-  "to": {
-    "value": Array<EmailAddress>,
-    "html":string,
-    "text":string
+  '_id':  string;
+  'timestamp':  Number;
+  'attachments':  Array<any>;
+  'headers':  any;
+  'html':  string;
+  'text': string,
+  'textAsHtml': string;
+  'subject': string;
+  'date': string;
+  'to':  {
+    'value':  Array<EmailAddress>;
+    'html': string;
+    'text': string;
+  };
+  'from': {
+    'value': Array<EmailAddress>;
+    'html': string;
+    'text': string;
   },
-  "from":{
-    "value":Array<EmailAddress>,
-    "html":string,
-    "text":string
-  },
-  "cc":{
-    "value":Array<EmailAddress>,
-    "html":string,
-    "text":string
+  'cc': {
+    'value': Array<EmailAddress>;
+    'html': string;
+    'text': string;
   }
-  "messageId":"<731871427.46.1499261090686.JavaMail.watchdox@localhost.localdom>"
+  'messageId': string;
 }
 
 
 export interface EmailAddress
   {
-    "address":string,
-    "name":string
+    'address': string;
+    'name': string;
   }
 
 
