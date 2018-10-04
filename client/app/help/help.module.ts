c881dac6d2de10419776a74dc93af87593459f06
ogeva <ogeva@blackberry.com>
2018-10-05 00:13:34 +0300

    faq

--- client/app/help/help.module.ts
+++ client/app/help/help.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HelpRoutingModule } from './help-routing.module';
 import {ApiDocumentationComponent} from './api-documentation/api-documentation.component';
 import {SharedModule} from '../shared/shared.module';
-import {MatCardModule, MatTableModule} from '@angular/material';
+import {MatCardModule, MatDividerModule, MatTableModule} from '@angular/material';
 import { FaqComponent } from './faq/faq.component';
 import {MomentModule} from 'ngx-moment';
 
@@ -14,6 +14,7 @@ import {MomentModule} from 'ngx-moment';
     SharedModule,
     MatTableModule,
     MatCardModule,
+    MatDividerModule,
     MomentModule,
     HelpRoutingModule
   ],
