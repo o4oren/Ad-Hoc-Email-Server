5812b1e25576ec4f0cf0c5f8a41e72eaf35dc9ad
ogeva <ogeva@blackberry.com>
2018-10-07 01:10:27 +0300

    always log to console

--- server/app/logger.js
+++ server/app/logger.js
@@ -8,16 +8,11 @@ const logger = winston.createLogger({
     // - Write to all logs with level `info` and below to `combined.log`
     // - Write all logs error (and below) to `error.log`.
     //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
+    new winston.transports.Console({
+      format: winston.format.simple()
+    }),
   ]
 });
-
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
-}
 
 module.exports = logger;
 module.exports.stream =  {
