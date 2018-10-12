61992e3c57dcb4accc7a0b3b7d4f6382d52b93bf
ogeva <ogeva@blackberry.com>
2018-10-12 11:58:16 +0300

    docs

--- client/app/help/api-documentation/api-documentation.component.ts
+++ client/app/help/api-documentation/api-documentation.component.ts
@@ -11,38 +11,67 @@ export class ApiDocumentationComponent implements OnInit {
   h1 = 'AHEM - Ad-Hoc Temporary Email Server API Documentation';
   h2 = '';
 
-  apiEndpoints: Endpoint[] = [
+  apiEndpoints: ApiCall[] = [
     {
-      httpMethod: 'POST', uriPath: '/api/auth/authenticate', parameters: '{}',
+      httpMethod: 'POST',
+      uriPath: '/auth/authenticate',
+      headers: [],
+      parameters: '{}',
       description: 'Returns an access token associated with the requesting IP address.'
     },
     {
-      httpMethod: 'POST', uriPath: '/api/account/autocomplete', parameters: '{ "prefix":[string] }',
+      httpMethod: 'POST',
+      uriPath: '/api/account/autocomplete',
+      headers: [],
+      parameters: '{ "prefix":[string] }',
       description: 'Returns a list of accounts starting with the prefix'
     },
-    {httpMethod: 'GET', uriPath: '/api/properties', parameters: '', description: 'Returns various server properties and settings'},
+    {
+      httpMethod: 'GET',
+      uriPath: '/api/properties',
+      headers: [],
+      parameters: '',
+      description: 'Returns various server properties and settings'},
     {
       httpMethod: 'GET',
       uriPath: '/api/account/{account}',
+      headers: [],
       parameters: '',
       description: 'returns a list of the email objects in the account'
     },
-    {httpMethod: 'DELETE', uriPath: '/api/account/{account}', parameters: '', description: 'Deletes a whole account'},
+    {
+      httpMethod: 'DELETE',
+      uriPath: '/api/account/{account}',
+      headers: [],
+      parameters: '',
+      description: 'Deletes a whole account'},
     {
       httpMethod: 'GET',
       uriPath: '/api/account/{account}/{emailId}',
+      headers: [],
       parameters: '',
       description: 'Returns the contents of a specific email'
     },
-    {httpMethod: 'DELETE', uriPath: '/api/account/{account}/{emailId}', parameters: '', description: 'Delete an emails'},
     {
-      httpMethod: 'PATCH', uriPath: '/api/account/{acount}/{emailId}', parameters: '{"isRead" : [boolean]}',
+      httpMethod: 'DELETE',
+      uriPath: '/api/account/{account}/{emailId}',
+      headers: [],
+      parameters: '',
+      description: 'Delete an emails'},
+    {
+      httpMethod: 'PATCH',
+      uriPath: '/api/account/{acount}/{emailId}',
+      headers: [],
+      parameters: '{"isRead" : [boolean]}',
       description: 'Updates the emailInfo object (representation of the email meta data in the user\'s account.'
       + ' Currently, only the isRead field is supported.'
     },
     {
-      httpMethod: 'GET', uriPath: '/api/account/{account}/{emailId}/attachments/{filename}',
-      parameters: '', description: 'Downloads a specific attachment on an email'
+      httpMethod: 'GET',
+      uriPath: '/api/account/{account}/{emailId}/attachments/{filename}',
+      headers: [],
+      parameters: '',
+      description: 'Downloads a specific attachment on an email'
     }
   ];
 
@@ -56,9 +85,10 @@ export class ApiDocumentationComponent implements OnInit {
 
 }
 
-export class Endpoint {
+export class ApiCall {
   httpMethod: string;
   uriPath: string;
+  headers: Array<string>;
   parameters: string;
   description: string;
 }
