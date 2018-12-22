386257be9db0b28fed12d9554faf9f2ba08133bc
ogeva <ogeva@blackberry.com>
2018-12-22 12:55:34 +0200

    typos

--- client/app/blog/blog/blog.component.ts
+++ client/app/blog/blog/blog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {BlogEntry} from '../model/blog-entry-model';
+import {BlogEntry} from '../../model/blog-entry-model';
 
 @Component({
   selector: 'app-blog',
@@ -16,7 +16,7 @@ export class BlogComponent implements OnInit {
     this.blogEntries = [
       {
         date: new Date('Nov 13, 2017'),
-        Title: 'How to setup you own disposable email server',
+        title: 'How to setup you own disposable email server',
         imageUrl: '/assets/images/happy-bluish.png',
         bodyUrl: '/assets/blog/how-to-set-up-your-own-disposable-email-server.html',
         bodyShortUrl: '/assets/blog/how-to-set-up-your-own-disposable-email-server.short.html'
