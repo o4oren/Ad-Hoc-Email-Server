15030a067ef567de496fb19b0041a1d99167261a
ogeva <ogeva@blackberry.com>
2019-01-08 01:10:40 +0200

    Don't try to set token or access local storage if platform is server

--- client/app/core/services/config.service.ts
+++ client/app/core/services/config.service.ts
@@ -17,7 +17,6 @@ export class ConfigService {
 
   load() {
     return new Promise<void>((resolve, reject) => {
-      console.log('========API/PROPERTIES======', this.baseUri)
       this.http.get(this.baseUri + '/api/properties')
         .toPromise().then(
         res => { // Success
--- client/app/core/services/token-interceptor.ts
+++ client/app/core/services/token-interceptor.ts
@@ -1,19 +1,25 @@
 ///<reference path="../../../../node_modules/@angular/common/http/src/interceptor.d.ts"/>
 import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from '@angular/common/http';
-import {Injectable} from '@angular/core';
+import {Inject, Injectable, PLATFORM_ID} from '@angular/core';
 import {Observable} from 'rxjs/internal/Observable';
 import { tap, mergeMap } from 'rxjs/operators';
 import {AuthService} from './auth.service';
 import {ApiService} from './api.service';
 import {EmailInfo} from '../../model/email-info-model';
+import {isPlatformServer} from '@angular/common';
 
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  constructor(private apiService: ApiService, private authService: AuthService) { }
+  constructor(private apiService: ApiService,
+              private authService: AuthService,
+              @Inject(PLATFORM_ID) private platformId: Object) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
+    if (isPlatformServer(this.platformId)) {
+      return next.handle(req);
+    }
 // Clone the request to add the new header if it is not an unauth call.
     if (req.url.includes('api/properties') || req.url.includes('assets')) {
       return next.handle(req);
@@ -33,7 +39,7 @@ export class TokenInterceptor implements HttpInterceptor {
             localStorage.setItem('access_token', res.token);
             authReq = req.clone({ headers: req.headers.set('Authorization', 'Bearer ' + this.authService.getToken())});
             return next.handle(authReq).pipe(tap((event: HttpEvent<any>) => {
-              console.log('event', event)
+              console.log('event', event);
               if (event instanceof HttpResponse && event.url.match(/\/api\/mailbox\/.+\/email$/g)) {
                 this.apiService.emails.next(event.body);
               }
