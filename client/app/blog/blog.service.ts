10f18dd4adc0c0e5ada012962eb14c208ac66476
ogeva <ogeva@blackberry.com>
2019-01-20 15:58:23 +0200

    reverse order of blog posts

--- client/app/blog/blog.service.ts
+++ client/app/blog/blog.service.ts
@@ -9,17 +9,17 @@ export class BlogService {
 
   blogEntries: Array<BlogEntry> = [
       {
+        date: new Date('Jan 17, 2019').toString(),
+        title: 'How to run AHEM server on docker',
+        imageUrl: '/assets/images/happy-bluish.png',
+        name: 'run-ahem-on-docker'
+      },
+      {
         date: new Date('Nov 13, 2018').toString(),
         title: 'How to setup you own disposable email server',
         imageUrl: '/assets/images/happy-bluish.png',
         name: 'how-to-set-up-your-own-disposable-email-server'
-      },
-    {
-      date: new Date('Jan 17, 2019').toString(),
-      title: 'How to run AHEM server on docker',
-      imageUrl: '/assets/images/happy-bluish.png',
-      name: 'run-ahem-on-docker'
-    }
+      }
     ];
 
   constructor() { }
