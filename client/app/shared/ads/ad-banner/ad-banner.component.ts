d299c56a090e9ca876aa9ac4c7d02e01d0e18f4b
Oren Geva <oren@fdna.com>
2020-02-01 22:53:40 +0200

    try appBanner...

--- client/app/shared/ads/ad-banner/ad-banner.component.ts
+++ client/app/shared/ads/ad-banner/ad-banner.component.ts
@@ -18,32 +18,31 @@ export class AdBannerComponent implements OnInit, AfterViewInit {
 
 
   ngAfterViewInit(): void {
-    const script = this._renderer2.createElement('script');
-    script.type = `text/javascript`;
-    script.text = `
-        atOptions = {
-                        'key' : '938d5c0fb87beb89eafe244059ced18c',
-                        'format' : 'iframe',
-                        'height' : 90,
-                        'width' : 728,
-                        'params' : {}
-                    };
-
-            var s = document.createElement('script');
-            s.type = 'text/javascript';
-            s.src = "https://www.madcpms.com/938d5c0fb87beb89eafe244059ced18c/invoke.js";
-            var code = "console.log('11111')"
-            try {
-              s.appendChild(document.createTextNode(code));
-              document.findElementById("adBanner").appendChild(s);
-            } catch (e) {
-              s.text = code;
-              document.body.appendChild(s);
-            }
-        `;
-    console.log(script)
-    this._renderer2.appendChild(this.adBanner.nativeElement, script);
-    // this.adBanner.nativeElement.insertAdjacentHTML('beforeend',script);
+    // const script = this._renderer2.createElement('script');
+    // script.type = `text/javascript`;
+    // script.text = `
+    //     atOptions = {
+    //                     'key' : '938d5c0fb87beb89eafe244059ced18c',
+    //                     'format' : 'iframe',
+    //                     'height' : 90,
+    //                     'width' : 728,
+    //                     'params' : {}
+    //                 };
+    //
+    //         var s = document.createElement('script');
+    //         s.type = 'text/javascript';
+    //         s.src = "https://www.madcpms.com/938d5c0fb87beb89eafe244059ced18c/invoke.js";
+    //         var code = "console.log('11111')"
+    //         try {
+    //           s.appendChild(document.createTextNode(code));
+    //           document.findElementById("adBanner").appendChild(s);
+    //         } catch (e) {
+    //           s.text = code;
+    //           document.body.appendChild(s);
+    //         }
+    //     `;
+    // this._renderer2.appendChild(this.adBanner.nativeElement, script);
+    // // this.adBanner.nativeElement.insertAdjacentHTML('beforeend',script);
   }
 
 }
