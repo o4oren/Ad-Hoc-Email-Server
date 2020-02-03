498e7423018c537a18dbe1a7de647064d94510cb
Oren Geva <oren@fdna.com>
2020-02-03 13:22:53 +0200

    adsterra not in iframe

--- client/app/shared/ads/ad-banner/ad-banner.component.ts
+++ client/app/shared/ads/ad-banner/ad-banner.component.ts
@@ -1,17 +1,48 @@
-import {Component, OnInit} from '@angular/core';
-import {DeviceService} from '../../../core/services/device.service';
+import {Component, OnInit, AfterViewInit, ViewChild, ElementRef, Renderer2, Inject} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-ad-banner',
   templateUrl: './ad-banner.component.html',
   styleUrls: ['./ad-banner.component.css']
 })
-export class AdBannerComponent implements OnInit {
+export class AdBannerComponent implements OnInit, AfterViewInit {
 
-  constructor(public deviceService: DeviceService) { }
+  @ViewChild('adBanner', {static: false, read: ElementRef}) adBanner: ElementRef;
+
+  constructor(private _renderer2: Renderer2) { }
 
   ngOnInit() {
 
+  }
+
+
+  ngAfterViewInit(): void {
+    const script = this._renderer2.createElement('script');
+    script.type = `text/javascript`;
+    script.text = `
+        atOptions = {
+                        'key' : '938d5c0fb87beb89eafe244059ced18c',
+                        'format' : 'iframe',
+                        'height' : 90,
+                        'width' : 728,
+                        'params' : {}
+                    };
+
+            var s = document.createElement('script');
+            s.type = 'text/javascript';
+            s.src = "https://www.madcpms.com/938d5c0fb87beb89eafe244059ced18c/invoke.js";
+            // s.code = 'console.log("aaaaa")'
+            try {
+              s.appendChild(document.createTextNode(code));
+              document.findElementById("adBanner").appendChild(s);
+            } catch (e) {
+              s.text = code;
+              document.body.appendChild(s);
+            }
+        `;
+    this._renderer2.appendChild(this.adBanner.nativeElement, script);
+    // this.adBanner.nativeElement.insertAdjacentHTML('beforeend',script);
   }
 
 }
