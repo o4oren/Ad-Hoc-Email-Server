64f7597f66f887f350b1b7fe3e39ae77e9266cdf
Oren Geva <o4oren@gmail.com>
2020-09-07 08:16:06 +0300

    try catch in splitting emails

--- server/app/smtp.js
+++ server/app/smtp.js
@@ -11,92 +11,96 @@ function startSTMPServer(properties, db, io) {
   const smtpPort = properties.smtpPort;
   logger.info('starting smtp on ' + properties.smtpPort);
   mailserver = new SMTPServer({
-      logger: false,
-      authOptional: true,
-      disabledCommands: ['AUTH'],
-      disableReverseLookup: true,
-      maxClients: 5,
-      onConnect(session, callback) {
-        logger.info('SMTP Connect from ' + session.remoteAddress);
-        return callback(); // Accept the connection
-      },
-      onMailFrom(address, session, callback) {
-        logger.info('SMTP MAIL FROM: ' + address.address);
-        return callback();
-      },
-      onRcptTo(address, session, callback) {
-        logger.info('SMTP RCPT TO: ' + address.address);
-        if (!validateAddress(address, properties.allowedDomains)) {
-          logger.error(address + ' is not allowed!');
-          return callback(new Error('Only the domains ' + [JSON.stringify(properties.allowedDomains)] + ' are allowed to receive mail'));
-        }
-        return callback(); // Accept the address
-      },
-      onData(stream, session, callback) {
-        logger.info('SMTP DATA start');
-        let mailDataString = '';
-
-        stream.on('data', function (chunk) {
-          mailDataString += chunk;
-        });
-
-        stream.on('end', function () {
-          logger.info('SMTP DATA end');
-          simpleParser(mailDataString, (err, mail) => {
-            mail.timestamp = new Date().getTime();
-
-            // replace header map with one in which  . in the header keys are changed to _ due to insertion probelm
-            mail.headers.forEach(function (value, key) {
-              if (key.includes('.')) {
-                const newkey = key.replace(/\./g, '_');
-                mail.headers.set(newkey, mail.headers.get(key));
-                mail.headers.delete(key);
-              }
-            });
-
-            db.collection('emails').insertOne(mail, function (err1, result) {
-
-              if (err1) {
-                logger.error('Error in writing email to db!', err1);
-                return;
-              }
-
-              // count email
-              db.collection('emailCount').findOneAndUpdate({}, {$inc: {count: 1}, $setOnInsert: { since: new Date().getTime() }}, {upsert: true, returnNewDocument: true}, (err, result) => {
-                io.emit('emailCount', result.value);
-              });
-              mail.to.value.forEach(recipient => {
-                try {
-                  const nameAndDomain = recipient.address.split('@');
-                  if (properties.allowedDomains.indexOf(nameAndDomain[1].toLowerCase()) > -1) {
-                    db.collection('mailboxes').updateOne({'name': nameAndDomain[0].toLowerCase()}, {
-                      $push: {
-                        'emails': {
-                          'emailId': mail._id,
-                          'sender': mail.from.value[0],
-                          'subject': mail.subject,
-                          'timestamp': mail.timestamp,
-                          'isRead': false
-                        }
-                      }
-                    }, {upsert: true}, function (err2, res) {
-                      if (err2) {
-                        logger.error('Error in writing to mailbox db', err2);
-                        return;
-                      }
-                      logger.info('updated email content in db.');
-                    });
-                  }
-                } catch (e) {
-                  logger.error(e);
-                }
-              });
-            });
-          });
-          return callback();
-        });
+    logger: false,
+    authOptional: true,
+    disabledCommands: ['AUTH'],
+    disableReverseLookup: true,
+    maxClients: 5,
+    onConnect(session, callback) {
+      logger.info('SMTP Connect from ' + session.remoteAddress);
+      return callback(); // Accept the connection
+    },
+    onMailFrom(address, session, callback) {
+      logger.info('SMTP MAIL FROM: ' + address.address);
+      return callback();
+    },
+    onRcptTo(address, session, callback) {
+      logger.info('SMTP RCPT TO: ' + address.address);
+      if (!validateAddress(address, properties.allowedDomains)) {
+        logger.error(address + ' is not allowed!');
+        return callback(new Error('Only the domains ' + [JSON.stringify(properties.allowedDomains)] + ' are allowed to receive mail'));
       }
-    });
+      return callback(); // Accept the address
+    },
+    onData(stream, session, callback) {
+      logger.info('SMTP DATA start');
+      let mailDataString = '';
+
+      stream.on('data', function (chunk) {
+        mailDataString += chunk;
+      });
+
+      stream.on('end', function () {
+        logger.info('SMTP DATA end');
+        simpleParser(mailDataString, (err, mail) => {
+          mail.timestamp = new Date().getTime();
+
+          // replace header map with one in which  . in the header keys are changed to _ due to insertion probelm
+          mail.headers.forEach(function (value, key) {
+            if (key.includes('.')) {
+              const newkey = key.replace(/\./g, '_');
+              mail.headers.set(newkey, mail.headers.get(key));
+              mail.headers.delete(key);
+            }
+          });
+
+          db.collection('emails').insertOne(mail, function (err1, result) {
+
+            if (err1) {
+              logger.error('Error in writing email to db!', err1);
+              return;
+            }
+
+            // count email
+            db.collection('emailCount').findOneAndUpdate({}, {
+              $inc: {count: 1},
+              $setOnInsert: {since: new Date().getTime()}
+            }, {upsert: true, returnNewDocument: true}, (err, result) => {
+              io.emit('emailCount', result.value);
+            });
+            try {
+              mail.to.value.forEach(recipient => {
+                const nameAndDomain = recipient.address.split('@');
+                if (properties.allowedDomains.indexOf(nameAndDomain[1].toLowerCase()) > -1) {
+                  db.collection('mailboxes').updateOne({'name': nameAndDomain[0].toLowerCase()}, {
+                    $push: {
+                      'emails': {
+                        'emailId': mail._id,
+                        'sender': mail.from.value[0],
+                        'subject': mail.subject,
+                        'timestamp': mail.timestamp,
+                        'isRead': false
+                      }
+                    }
+                  }, {upsert: true}, function (err2, res) {
+                    if (err2) {
+                      logger.error('Error in writing to mailbox db', err2);
+                      return;
+                    }
+                    logger.info('updated email content in db.');
+                  });
+                }
+              })
+            } catch (e) {
+              logger.error(e);
+            }
+            ;
+          });
+        });
+        return callback();
+      });
+    }
+  });
 
   mailserver.on('error', err => {
     logger.error('Error %s', err.message);
